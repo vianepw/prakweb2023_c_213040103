ea3ae22b9b75f07a7555ad6c2b375e232092cb81
vianepw <viane.213040103@mail.unpas.ac.id>
2023-10-17 18:06:26 +0700

    Pertemuan 3

---
+++ bukuku/hapus.php
@@ -0,0 +1,13 @@
+<?php 
+
+require 'function.php';
+$id = $_GET['id'];
+
+if(hapus($id) > 0 ) {
+    echo "<script>
+            alert('data berhasil dihapus!');
+            document.location.href = 'index.php';
+          </script>";
+}
+
+?>
